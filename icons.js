b5a857206a8e5d401195c5586f1848fdbd5ebb45
Jacob Wilson <Jacobleeeugenewilson@gmail.com>
2024-05-04 23:32:18 -0700

    Add icons

---
+++ icons.js
@@ -0,0 +1,21 @@
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+const D4 = <Icon name="dice-d4" size={30} color="#000000" />;
+const D6 = <Icon name="dice-d6" size={30} color="#000000" />;
+const D8 = <Icon name="dice-d8" size={30} color="#000000" />;
+const D10 = <Icon name="dice-d10" size={30} color="#000000" />;
+const D12 = <Icon name="dice-d12" size={30} color="#000000" />;
+const D20 = <Icon name="dice-d20" size={30} color="#000000" />;
+const D100 = <Icon name="cash-100" size={35} color="#000000" />;
+
+const dice = {
+  D4,
+  D6,
+  D8,
+  D10,
+  D12,
+  D20,
+  D100,
+};
+
+export default dice;
