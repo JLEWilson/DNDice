47be0e0c0e92d4406a9c73aa45f35b4f598e613b
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2024-06-14 12:03:34 -0700

    add diceRolls

--- App/Utils.ts
+++ App/Utils.ts
@@ -1,14 +1,22 @@
-import { Dice } from "../db"
+import { Dice, DiceRolls } from "../db"
 
 export const MacroRandomizer = (macro: Dice) => {
-    let min = 0;
     let max = 0;
+    let allRolls: DiceRolls = {
+        D4: [],
+        D6: [],
+        D8: [],
+        D10: [],
+        D12: [],
+        D20: [],
+        D100: [],
+    }
 
     Object.entries(macro).map(([key, value]) => {
-        min += value;
         const keyAsNumber = parseInt(key.substring(1));
-        const y = keyAsNumber * value
-        max += y
+        for (let index = 0; index < value; index++) {
+            allRolls[key as keyof DiceRolls].push((Math.floor(Math. random() * (keyAsNumber) + 1)))
+        }
     })
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return allRolls
 }
