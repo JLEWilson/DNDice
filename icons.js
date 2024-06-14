ecc3cfe4208ea9c0d9cdc15faa06e05d8e8d0b43
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2024-06-14 12:03:25 -0700

    add + - icons

--- icons.js
+++ icons.js
@@ -6,7 +6,9 @@ const D8 = <Icon name="dice-d8" size={30} color="#000000" />;
 const D10 = <Icon name="dice-d10" size={30} color="#000000" />;
 const D12 = <Icon name="dice-d12" size={30} color="#000000" />;
 const D20 = <Icon name="dice-d20" size={30} color="#000000" />;
-const D100 = <Icon name="cash-100" size={35} color="#000000" />;
+const D100 = <Icon name="cash-100" size={30} color="#000000" />;
+const addValue = <Icon name="numeric-positive-1" size={30} color="#000000"/>
+const subtractValue = <Icon name="numeric-negative-1" size={30} color="#000000"/>
 
 const DiceIcons = {
   D4,
@@ -17,5 +19,9 @@ const DiceIcons = {
   D20,
   D100,
 };
+const ValueIcons = {
+  addValue,
+  subtractValue
+}
 
-export default DiceIcons;
+export {DiceIcons, ValueIcons}
