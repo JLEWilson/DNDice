b9147576db0b415b1861d0b3f8e2338d8ea0a9f9
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2024-06-14 12:03:05 -0700

    Styling updates

--- App/Landing.tsx
+++ App/Landing.tsx
@@ -1,12 +1,12 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, TextInput, ScrollView } from "react-native";
 import React from "react";
 import { StyleSheet } from "react-native";
-import DiceIcons from "../icons";
-import { Dice } from "../db";
+import { DiceIcons, ValueIcons } from "../icons";
+import { Dice, DiceRolls } from "../db";
 import { MacroRandomizer } from "./Utils";
 
 export default function Landing() {
- const [macro, setMacro] = React.useState<Dice>({
+  const [macro, setMacro] = React.useState<Dice>({
     D4: 0,
     D6: 0,
     D8: 0,
@@ -15,11 +15,99 @@ export default function Landing() {
     D20: 0,
     D100: 0,
   });
-  const [roll, setRoll] = React.useState<number>(0)
+  const [rolls, setRolls] = React.useState<DiceRolls>({
+    D4: [],
+    D6: [],
+    D8: [],
+    D10: [],
+    D12: [],
+    D20: [],
+    D100: [],
+  });
+  const [allRolls, setAllRolls] = React.useState<DiceRolls[]>([]);
+  const [increment, setIncrement] = React.useState<Number>();
+  const [showIncrement, setShowIncrement] = React.useState(false);
+  const [decrement, setDecrement] = React.useState<Number>();
+  const [showDecrement, setShowDecrement] = React.useState(false);
+  const [formattedRolls, setFormattedRolls] =  React.useState<JSX.Element[]>([]);
+
+
+  const clearDecrement = () => {
+    setShowDecrement(false);
+    setDecrement(0);
+  };
+  const clearIncrement = () => {
+    setShowIncrement(false);
+    setIncrement(0);
+  };
+
+  const hasNumbers = (diceRolls: DiceRolls) => {
+    if (Array.isArray(diceRolls)) {
+      return diceRolls.length > 0;
+    } else {
+      return Object.values(diceRolls).some((dice) => dice.length > 0);
+    }
+  };
+
+  const total = hasNumbers(rolls) ? sumAllDiceRolls(rolls) : null;
 
   const rollMacro = (macro: Dice) => {
-    const x = MacroRandomizer(macro)
-    setRoll(x)
+    const x = MacroRandomizer(macro);
+    setRolls(x);
+    setAllRolls((prevAllRolls) => [...prevAllRolls, x]);
+  };
+
+  const formatRolls = (numbers: number[]) => {
+    if (numbers.length === 0) {
+      return "";
+    } else if (numbers.length === 1) {
+      return numbers[0].toString();
+    } else {
+      return numbers.join(", ");
+    }
+  };
+
+  const formatAllRolls = (allRolls: DiceRolls[]) => {
+    const rollEntries: JSX.Element[] = allRolls.map((rollSet, index) => {
+      const totalForSet = sumAllDiceRolls(rollSet);
+      return (
+        <View key={index}>
+          {Object.keys(rollSet).map((key) => {
+            if (rollSet[key as keyof DiceRolls].length > 0) {
+              return (
+                <View key={key}>
+                  <Text>{`${key}: ${formatRolls(rollSet[key as keyof DiceRolls])}`}</Text>
+                </View>
+              );
+            }
+            return null;
+          })}
+          <Text>{`Total: ${totalForSet}`}</Text>
+        </View>
+      );
+    });
+
+    setFormattedRolls(rollEntries);
+  };
+
+  React.useEffect(() => {
+    formatAllRolls(allRolls);
+  }, [allRolls]);
+
+  function sumAllDiceRolls(diceRolls: DiceRolls): number {
+    let sum = 0;
+
+    if (Array.isArray(diceRolls)) {
+      // If diceRolls is a single array of numbers
+      sum = diceRolls.reduce((acc: number, val: number) => acc + val, 0);
+    } else {
+      // If diceRolls is an object of arrays of numbers
+      Object.values(diceRolls).forEach((dice) => {
+        sum += dice.reduce((acc: number, val: number) => acc + val, 0);
+      });
+    }
+
+    return sum;
   }
 
   const incrementMacro = (key: keyof Dice) => {
@@ -38,43 +126,133 @@ export default function Landing() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.diceContainer}>
-        {Object.entries(DiceIcons).map(([key, value]) => (
+
+      <View style={styles.box1}>
+        <View style={styles.diceContainer}>
+          {Object.entries(DiceIcons).map(([key, value]) => (
+            <Pressable
+              key={key}
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
+                },
+                styles.button,
+              ]}
+              onPress={() => incrementMacro(key as keyof Dice)}
+            >
+              {value}
+            </Pressable>
+          ))}
+        </View>
+        <View style={styles.diceIncrementDecrement}>
           <Pressable
-            key={key}
+            key={"showIncrementButton"}
             style={({ pressed }) => [
               {
                 backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
               },
               styles.button,
             ]}
-            onPress={() => incrementMacro(key as keyof Dice)}
+            onPress={() => setShowIncrement(true)}
           >
-            {value}
+            {ValueIcons.addValue}
           </Pressable>
-        ))}
+          <Pressable
+            key={"showDecrementButton"}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
+              },
+              styles.button,
+            ]}
+            onPress={() => setShowDecrement(true)}
+          >
+            {ValueIcons.addValue}
+          </Pressable>
+        </View>
       </View>
+
       <Text style={styles.macroTitle}>Macro</Text>
-      <View style={styles.macroContainer}>
-        {Object.entries(macro).map(([key, value]) => (
-          value > 0 &&
-          <Pressable key={key} style={styles.macroItem} onPress={() => decrementMacro(key as keyof Dice)}>
-            {DiceIcons[key as keyof typeof DiceIcons]}
-            <Text>{`: ${value}`}</Text>
+      <View style={styles.box2}>
+        <View style={styles.macroContainer}>
+          {Object.entries(macro).map(
+            ([key, value]) =>
+              value > 0 && (
+                <Pressable
+                  key={key}
+                  style={styles.macroItem}
+                  onPress={() => decrementMacro(key as keyof Dice)}
+                >
+                  {DiceIcons[key as keyof typeof DiceIcons]}
+                  <Text>{`: ${value}`}</Text>
+                </Pressable>
+              )
+          )}
+          {showDecrement && (
+            <View style={styles.plusMinusContainer}>
+              <Pressable
+                key={"DecrementFalse"}
+                style={({ pressed }) => [
+                  {
+                    backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
+                  },
+                  styles.button,
+                ]}
+                onPress={() => clearDecrement()}
+              >
+                {ValueIcons.addValue}
+              </Pressable>
+              <TextInput
+                style={styles.textInput}
+                keyboardType="numeric"
+                onChangeText={(text) => setDecrement(parseInt(text))}
+                value={decrement?.toString()}
+                maxLength={4}
+              />
+            </View>
+          )}
+          {showIncrement && (
+            <View style={styles.plusMinusContainer}>
+              <Pressable
+                key={"IncrementFalse"}
+                style={({ pressed }) => [
+                  {
+                    backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
+                  },
+                  styles.button,
+                ]}
+                onPress={() => clearIncrement()}
+              >
+                {ValueIcons.addValue}
+              </Pressable>
+              <TextInput
+                style={styles.textInput}
+                keyboardType="numeric"
+                onChangeText={(text) => setIncrement(parseInt(text))}
+                value={increment?.toString()}
+                maxLength={4}
+              />
+            </View>
+          )}
+        </View>
+      </View>
+
+      <View style={styles.box3}>
+        <ScrollView
+          style={styles.scrollContainer}
+          // contentContainerStyle={styles.scrollContent}
+        >
+          <View>
+            {formattedRolls}
+          </View>
+        </ScrollView>
+      </View>  
+      
+      <View style={styles.box4}>
+          <Pressable style={styles.rollButton} onPress={() => rollMacro(macro)}>
+            <Text style={styles.rollButtonText}>Roll</Text>
           </Pressable>
-        ))}
-      </View>
-      <View style={styles.buttonContainer}>
-          <Pressable onPress={() => rollMacro(macro)}>
-            <Text>Roll</Text>
-          </Pressable>
-      </View>
-      <View style={styles.outputContainer}>
-        {
-          roll > 0 &&
-          <Text style={styles.outputText}>{roll}</Text>
-        }
-      </View>
+        </View>
     </View>
   );
 }
@@ -82,28 +260,54 @@ export default function Landing() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 5,
+    marginHorizontal: 6,
+    marginVertical: 15,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
   diceContainer: {
-    flex: 1,
-    backgroundColor: "#fff",
+    flex: 2,
+    backgroundColor: "green",
     flexDirection: "row",
-    alignItems: "center",
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  diceIncrementDecrement: {
+    flex: 1,
+    backgroundColor: "red",
+    flexDirection: "row",
+    alignItems: "flex-start",
     justifyContent: "center",
   },
   macroContainer: {
     flex: 1,
     flexWrap: "wrap",
-    backgroundColor: "#fff",
     flexDirection: "row",
     alignItems: "flex-start",
     justifyContent: "center",
   },
+  box1: {
+    flex: 1,
+    alignSelf: "stretch",
+    gap: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+    box2: {
+      flex: 1,
+      alignSelf: "stretch",
+      backgroundColor: "red",
+    },
   macroTitle: {
     fontSize: 20,
-    marginRight: "auto"
+    marginRight: "auto",
+  },
+  plusMinusContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   macroItem: {
     display: "flex",
@@ -120,19 +324,55 @@ const styles = StyleSheet.create({
     marginHorizontal: 2,
     padding: 5,
   },
-  buttonContainer: {
+  box4: {
     flex: 1,
-    backgroundColor: "#fff",
+    alignSelf: "stretch",
+    backgroundColor: "blue",
     alignItems: "center",
     justifyContent: "center",
-  }, 
-  outputContainer: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  rollButton: {
+    width: 100,
+    height: 50,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+    justifyContent: "center"
+    },
+  rollButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+    textAlign: "center"
+  },
+  box3: {
+    flex: 3,
+    alignSelf: "stretch",
+    alignItems: "flex-start",
+    borderWidth: 3,
+    borderColor: "Black",
+    backgroundColor: "blue",
     justifyContent: "center",
-  }, 
-  outputText : {
-    fontSize: 40
-  }
+  },
+  scrollContainer: {
+    flex: 1,
+    paddingHorizontal: 35,
+    alignSelf: "stretch",
+  },
+  scrollContent: {
+    flex: 1,
+    alignSelf: "stretch",
+  },
+  textInput: {
+    height: 40,
+    width: 50,
+    fontWeight: "bold",
+    marginTop: 4,
+    borderWidth: 1,
+    padding: 10,
+    textAlign: "center",
+  },
 });
