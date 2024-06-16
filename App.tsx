45827e9c13ec5e81c54c5404c107070ee57a9e71
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2024-06-15 23:53:01 -0700

    add tab navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Landing from "./App/Landing";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MyTabBar from "./App/MyTabBar"
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'darkolivegreen'
+  },
+};
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Landing />
-    </View>
+    <NavigationContainer theme={MyTheme}>
+      <Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
+        <Tab.Screen name="Macro" component={Landing} options={{headerShown: false}}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
